094c8f2fca0494b1b9d209b89bcf2070c963af5e
Deot <zrd0921@qq.com>
2018-09-10 10:44:35 +0800

    update demo

--- ios/examples/WYAWebViewDemo/ViewController.swift
+++ ios/examples/WYAWebViewDemo/ViewController.swift
@@ -9,60 +9,40 @@
 import UIKit
 import WYAWebView
 
+fileprivate let DEMO_URL = "https://wya-team.github.io/hybrid-sdk/html5/examples/dist/"
+
 class ViewController: UIViewController {
     
-    let web = WYAWebView()
-    
+    let h5 = WYAWebView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        h5.frame = self.view.bounds
         
+        // 配置项
+        h5.config()
         
-        web.frame = self.view.bounds
+        // 打开网页
+        h5.loadUrl(url: DEMO_URL)
         
-        web.configWebView()
+        // 打开本地文件
+        // web.loadLocalHtml(htmlName: "index")
         
+        self.view.addSubview(h5)
         
-        
-        web.loadUrl(url: "https://wya-team.github.io/hybrid-sdk/html5/examples/dist/")
-//        web.loadUrl(url: "https://www.baidu.com")
-//        web.loadLocalHtml(htmlName: "q1")
-        self.view.addSubview(web)
-        web.vc = self
-        
-        
-        
-        let system = SystemConfig()
-        let aa = system.appId
-        print(aa)
-        
+        h5.vc = self
     }
-    
-    
-    
-    //开始摇晃
-    override func motionBegan(_ motion: UIEventSubtype, with event: UIEvent?) {
-        print("开始摇晃")
-        web.shakeStateAction(shakeState: .shakeStart)
-    }
-    //摇晃结束
-    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
-        print("摇晃结束")
-        web.shakeStateAction(shakeState: .shakeEnd)
-    }
-    //摇晃被意外终止
-    override func motionCancelled(_ motion: UIEventSubtype, with event: UIEvent?) {
-        print("摇晃被意外终止")
-        web.shakeStateAction(shakeState: .shakeCancel)
-    }
-    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+}
+
+// MARK: --
+extension ViewController {
+
 }
 
 
