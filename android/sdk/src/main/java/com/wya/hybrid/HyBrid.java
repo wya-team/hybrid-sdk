8c8cc279732069ea6e825c1b97a75e3f1ad6121d
MaoShine <550612711@qq.com>
2019-01-17 10:20:06 +0800

    android_develop分支创建

--- android/sdk/src/main/java/com/wya/hybrid/HyBrid.java
+++ android/sdk/src/main/java/com/wya/hybrid/HyBrid.java
@@ -1,16 +1,10 @@
 package com.wya.hybrid;
 
-import com.wya.uikit.toolbar.BaseToolBarActivity;
-
 /**
  * @date: 2019/1/16 16:09
  * @author: Chunjiang Mao
  * @classname: HyBrid
  * @describe:
  */
-public class HyBrid extends BaseToolBarActivity {
-	@Override
-	protected int getLayoutId() {
-		return 0;
-	}
+public class HyBrid {
 }
