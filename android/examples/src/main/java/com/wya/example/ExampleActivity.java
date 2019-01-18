9d663acf02eb494c6052c3bfb08698ec16ea0ce8
XuDonglin <465715784@qq.com>
2019-01-18 14:09:33 +0800

    fix bugs

--- android/examples/src/main/java/com/wya/example/ExampleActivity.java
+++ android/examples/src/main/java/com/wya/example/ExampleActivity.java
@@ -3,6 +3,7 @@ package com.wya.example;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.wya.hybrid.JsCallBack;
 import com.wya.hybrid.WYAWebView;
@@ -14,7 +15,7 @@ import com.wya.hybrid.WYAWebView;
  * @describe:
  */
 public class ExampleActivity extends AppCompatActivity {
-	private static final String HTML_PATH = "https://wya-team.github.io/hybrid-sdk/html5/examples/dist";
+	private static final String HTML_PATH = "https://wya-team.github.io/hybrid-sdk/html5/examples/dist/";
 
 	private WYAWebView webView;
 	private ProgressBar progressBar;
@@ -26,11 +27,10 @@ public class ExampleActivity extends AppCompatActivity {
 		webView = findViewById(R.id.webView);
 		progressBar = findViewById(R.id.progress_bar);
 		webView.loadUrl(HTML_PATH);
-//		webView.emit(Events.KEY_BACK);
 		webView.register("debugger", new JsCallBack() {
 			@Override
 			public void response(String data, int id) {
-
+				Toast.makeText(ExampleActivity.this, data, Toast.LENGTH_SHORT).show();
 			}
 		});
 	}
