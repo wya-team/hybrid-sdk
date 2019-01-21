0e0cb40a39c369a23854fb0551fda700cd0c35a2
MaoShine <550612711@qq.com>
2019-01-21 11:44:26 +0800

    软件盘监听加按钮监听事件

--- android/examples/src/main/java/com/wya/hybridexample/ExampleActivity.java
+++ android/examples/src/main/java/com/wya/hybridexample/ExampleActivity.java
@@ -4,9 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.widget.ProgressBar;
 
 import com.wya.hybrid.WYAWebView;
+import com.wya.hybrid.bean.KeyBack;
+import com.wya.hybrid.bean.VolumeDown;
+import com.wya.hybrid.bean.VolumeUp;
 import com.wya.hybridexample.base.ActivityManager;
 import com.wya.hybridexample.data.event.ForegroundEvent;
 import com.wya.hybridexample.data.sp.ForegroundStateSP;
@@ -23,117 +27,162 @@ import org.greenrobot.eventbus.EventBus;
  * @describe:
  */
 public class ExampleActivity extends AppCompatActivity implements PermissionCallback {
-    private static final String HTML_PATH = "https://wya-team.github.io/hybrid-sdk/html5/examples/dist/";
-    
-    private WYAWebView mWebView;
-    private ProgressBar progressBar;
-    private EventManager mEventManager;
-    
-    /**
-     * permission
-     */
-    protected PermissionCheck permissionHelper;
-    
-    private String[] REQUEST_PERMISSIONS = new String[]{
-            android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            android.Manifest.permission.READ_PHONE_STATE,
-            android.Manifest.permission.DISABLE_KEYGUARD
-    };
-    
-    private boolean mIsFromBackground = false;
-    
-    private void checkPermission() {
-        permissionHelper.request(REQUEST_PERMISSIONS);
-    }
-    
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_example);
-        
-        permissionHelper = PermissionCheck.getInstance(this);
-        checkPermission();
-        
-        // webView
-        mWebView = findViewById(R.id.webView);
-        progressBar = findViewById(R.id.progress_bar);
-        mWebView.loadUrl(HTML_PATH);
-        
-        // event manager
-        mEventManager = new EventManager(this, mWebView);
-        
-    }
-    
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        permissionHelper.onActivityForResult(requestCode);
-    }
-    
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        permissionHelper.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    }
-    
-    @Override
-    public void onPermissionGranted(PermissionCheck permissionCheck, String[] strings) {
-        // TODO: 2019/1/19 ZCQ TEST
-    }
-    
-    @Override
-    public void onPermissionDeclined(PermissionCheck permissionCheck, final String[] permissionName) {
-        // TODO: 2019/1/19 ZCQ TEST
-    }
-    
-    @Override
-    public void onPermissionNeedExplanation(PermissionCheck permissionCheck, final String permissionName) {
-        // TODO: 2019/1/19 ZCQ TEST
-    }
-    
-    @Override
-    public void onPermissionReallyDeclined(PermissionCheck permissionCheck, final String[] permissionName) {
-        // TODO: 2019/1/19 ZCQ TEST
-    }
-    
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (ActivityManager.getInstance().isForeground() && mIsFromBackground) {
-            DebugLogger.logEvent("onResume ... ");
-            mIsFromBackground = false;
-            ForegroundStateSP.get().setIsResume(true);
-            ForegroundStateSP.get().setIsPause(false);
-            
-            ForegroundEvent event = new ForegroundEvent();
-            event.setPause(false);
-            event.setResume(true);
-            EventBus.getDefault().post(event);
-        }
-    }
-    
-    @Override
-    public void onStop() {
-        super.onStop();
-        if (!ActivityManager.getInstance().isForeground()) {
-            DebugLogger.logEvent("onStop ... ");
-            
-            mIsFromBackground = true;
-            ForegroundStateSP.get().setIsPause(true);
-            ForegroundStateSP.get().setIsResume(false);
-            
-            ForegroundEvent event = new ForegroundEvent();
-            event.setPause(true);
-            event.setResume(false);
-            EventBus.getDefault().post(event);
-        }
-    }
-    
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        // event manager
-        if (null != mEventManager) {
-            mEventManager.release();
-        }
-    }
-    
+	private static final String HTML_PATH = "https://wya-team.github.io/hybrid-sdk/html5/examples/dist/";
+
+	private WYAWebView mWebView;
+	private ProgressBar progressBar;
+	private EventManager mEventManager;
+
+	/**
+	 * permission
+	 */
+	protected PermissionCheck permissionHelper;
+
+	private String[] REQUEST_PERMISSIONS = new String[]{
+		android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+		android.Manifest.permission.READ_PHONE_STATE,
+		android.Manifest.permission.DISABLE_KEYGUARD
+	};
+
+	private boolean mIsFromBackground = false;
+
+	private void checkPermission() {
+		permissionHelper.request(REQUEST_PERMISSIONS);
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_example);
+
+		permissionHelper = PermissionCheck.getInstance(this);
+		checkPermission();
+
+		// webView
+		mWebView = findViewById(R.id.webView);
+		progressBar = findViewById(R.id.progress_bar);
+		mWebView.loadUrl(HTML_PATH);
+
+		// event manager
+		mEventManager = new EventManager(this, mWebView);
+
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		permissionHelper.onActivityForResult(requestCode);
+	}
+
+	@Override
+	public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+		permissionHelper.onRequestPermissionsResult(requestCode, permissions, grantResults);
+	}
+
+	@Override
+	public void onPermissionGranted(PermissionCheck permissionCheck, String[] strings) {
+		// TODO: 2019/1/19 ZCQ TEST
+	}
+
+	@Override
+	public void onPermissionDeclined(PermissionCheck permissionCheck, final String[] permissionName) {
+		// TODO: 2019/1/19 ZCQ TEST
+	}
+
+	@Override
+	public void onPermissionNeedExplanation(PermissionCheck permissionCheck, final String permissionName) {
+		// TODO: 2019/1/19 ZCQ TEST
+	}
+
+	@Override
+	public void onPermissionReallyDeclined(PermissionCheck permissionCheck, final String[] permissionName) {
+		// TODO: 2019/1/19 ZCQ TEST
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		if (ActivityManager.getInstance().isForeground() && mIsFromBackground) {
+			DebugLogger.logEvent("onResume ... ");
+			mIsFromBackground = false;
+			ForegroundStateSP.get().setIsResume(true);
+			ForegroundStateSP.get().setIsPause(false);
+
+			ForegroundEvent event = new ForegroundEvent();
+			event.setPause(false);
+			event.setResume(true);
+			EventBus.getDefault().post(event);
+		}
+	}
+
+	@Override
+	public void onStop() {
+		super.onStop();
+		if (!ActivityManager.getInstance().isForeground()) {
+			DebugLogger.logEvent("onStop ... ");
+
+			mIsFromBackground = true;
+			ForegroundStateSP.get().setIsPause(true);
+			ForegroundStateSP.get().setIsResume(false);
+
+			ForegroundEvent event = new ForegroundEvent();
+			event.setPause(true);
+			event.setResume(false);
+			EventBus.getDefault().post(event);
+		}
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		// event manager
+		if (null != mEventManager) {
+			mEventManager.release();
+		}
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		switch (keyCode) {
+			case KeyEvent.KEYCODE_BACK:
+				setKeyBack(keyCode);
+				return false;
+			case KeyEvent.KEYCODE_VOLUME_UP:
+				setVolumeUp(keyCode);
+				return false;
+			case KeyEvent.KEYCODE_VOLUME_DOWN:
+				setVolumeDown(keyCode);
+				return false;
+			case KeyEvent.KEYCODE_MENU:
+
+				return false;
+			default:
+				break;
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+
+	private void setVolumeDown(int keyCode) {
+		VolumeDown volumeDown = new VolumeDown();
+		volumeDown.setKeyCode(keyCode);
+		volumeDown.setLongPress(false);
+		mWebView.send(VolumeDown.EVENT_VOLUME_DOWN, volumeDown);
+	}
+
+	private void setVolumeUp(int keyCode) {
+		VolumeUp volumeUp = new VolumeUp();
+		volumeUp.setKeyCode(keyCode);
+		volumeUp.setLongPress(false);
+		mWebView.send(VolumeUp.EVENT_VOLUME_UP, volumeUp);
+	}
+
+	/**
+	 * 返回按钮
+	 * @param keyCode
+	 */
+	private void setKeyBack(int keyCode) {
+		KeyBack keyBack = new KeyBack();
+		keyBack.setKeyCode(keyCode);
+		keyBack.setLongPress(false);
+		mWebView.send(KeyBack.EVENT_KEY_BACK, keyBack);
+	}
 }
--- android/sdk/src/main/java/com/wya/hybrid/WYAWebView.java
+++ android/sdk/src/main/java/com/wya/hybrid/WYAWebView.java
@@ -1,19 +1,25 @@
 package com.wya.hybrid;
 
 import android.content.Context;
+import android.graphics.Rect;
 import android.os.Build;
 import android.text.TextUtils;
 import android.util.AttributeSet;
+import android.view.ViewTreeObserver;
 import android.webkit.WebView;
 
 import com.google.gson.Gson;
 import com.wya.hybrid.bean.BaseEmitData;
 import com.wya.hybrid.bean.InitBean;
+import com.wya.hybrid.bean.Keyboard;
 import com.wya.utils.utils.AppUtil;
 import com.wya.utils.utils.PhoneUtil;
+import com.wya.utils.utils.ScreenUtil;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import static com.wya.uikit.toolbar.StatusBarUtil.getStatusBarHeight;
 
 /**
  * @date: 2019/1/17 10:05
@@ -22,105 +28,148 @@ import java.util.Map;
  * @describe:
  */
 public class WYAWebView extends WebView {
-    private Map<String, JsCallBack> taskMap = new HashMap<>();
-    private BaseEmitData<InitBean> baseEmitData;
-    private Context mContext;
-    
-    public WYAWebView(Context context) {
-        super(context);
-        mContext = context;
-        init();
-    }
-    
-    public WYAWebView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        mContext = context;
-        init();
-    }
-    
-    public WYAWebView(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-        mContext = context;
-        init();
-    }
-    
-    private void init() {
-        this.setVerticalScrollBarEnabled(false);
-        this.setHorizontalScrollBarEnabled(false);
-        this.getSettings().setJavaScriptEnabled(true);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            WebView.setWebContentsDebuggingEnabled(true);
-        }
-        this.setWebViewClient(wyaWebViewClient());
-        this.setWebChromeClient(wyaWebChromeClient());
-        initData();
-    }
-    
-    private void initData() {
-        baseEmitData = new BaseEmitData<>();
-        InitBean initBean = new InitBean();
-        initBean.setAppId(AppUtil.getPackageName(mContext));
-        initBean.setAppName(AppUtil.getAppName(mContext));
-        initBean.setAppVersion(AppUtil.getVersionName(mContext));
-        initBean.setSystemType("android");
-        initBean.setSystemVersion(PhoneUtil.getInstance().getSDKVersion());
-        // TODO: 2019/1/19 ZCQ TEST
-        //        initBean.setDeviceId(PhoneUtil.getInstance().getPhoneImei(mContext));
-        initBean.setDeviceModel(PhoneUtil.getInstance().getPhoneModel());
-        initBean.setDeviceName(TextUtils.isEmpty(android.os.Build.DEVICE) ? "" : android.os.Build.DEVICE);
-        initBean.setUiMode(PhoneUtil.getInstance().isTablet(mContext) ? "pad" : "phone");
-        initBean.setOperatorName("移动");
-        initBean.setConnectionType("4g");
-        initBean.setScreenWidth(PhoneUtil.getInstance().getPhoneWidth(mContext));
-        initBean.setScreenHeight(PhoneUtil.getInstance().getPhoneHeight(mContext));
-        baseEmitData.setData(initBean);
-    }
-    
-    private WYAWebChromeClient wyaWebChromeClient() {
-        return new WYAWebChromeClient(this);
-    }
-    
-    private WYAWebViewClient wyaWebViewClient() {
-        return new WYAWebViewClient(this);
-    }
-    
-    public <T> void send(String name, T t) {
-        name = "'" + name + "'";
-        BaseEmitData<T> result = new BaseEmitData<>();
-        result.setData(t);
-        BridgeUtil.loadJsUrl(this, name, new Gson().toJson(result));
-    }
-    
-    public <T> void send(int id, T t) {
-        BaseEmitData<T> result = new BaseEmitData<>();
-        result.setData(t);
-        BridgeUtil.loadJsUrl(this, String.valueOf(id), new Gson().toJson(result));
-    }
-    
-    public void register(String name, JsCallBack callBack) {
-        taskMap.put(name, callBack);
-    }
-    
-    public void unRegister() {
-        taskMap.clear();
-        taskMap = null;
-    }
-    
-    public void handlerReturnData(String url) {
-        String[] split = url.split("[?]");
-        String name = split[0];
-        JsCallBack callBack = taskMap.get(name);
-        if (callBack != null) {
-            String id = split[1].replace("id=", BridgeUtil.EMPTY);
-            BridgeUtil.getParam(this, id, callBack);
-        }
-    }
-    
-    public BaseEmitData<InitBean> getBaseEmitData() {
-        return baseEmitData;
-    }
-    
-    public void setBaseEmitData(BaseEmitData<InitBean> baseEmitData) {
-        this.baseEmitData = baseEmitData;
-    }
+	private Map<String, JsCallBack> taskMap = new HashMap<>();
+	private BaseEmitData<InitBean> baseEmitData;
+	private Context mContext;
+	/**
+	 * 软键盘的显示状态
+	 */
+	private boolean ShowKeyboard;
+
+	public WYAWebView(Context context) {
+		super(context);
+		mContext = context;
+		init();
+	}
+
+	public WYAWebView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		mContext = context;
+		init();
+	}
+
+	public WYAWebView(Context context, AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+		mContext = context;
+		init();
+	}
+
+	private void init() {
+		this.setVerticalScrollBarEnabled(false);
+		this.setHorizontalScrollBarEnabled(false);
+		this.getSettings().setJavaScriptEnabled(true);
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+			WebView.setWebContentsDebuggingEnabled(true);
+		}
+		this.setWebViewClient(wyaWebViewClient());
+		this.setWebChromeClient(wyaWebChromeClient());
+		initData();
+		setKeyBoardListener();
+	}
+
+	private void initData() {
+		baseEmitData = new BaseEmitData<>();
+		InitBean initBean = new InitBean();
+		initBean.setAppId(AppUtil.getPackageName(mContext));
+		initBean.setAppName(AppUtil.getAppName(mContext));
+		initBean.setAppVersion(AppUtil.getVersionName(mContext));
+		initBean.setSystemType("android");
+		initBean.setSystemVersion(PhoneUtil.getInstance().getSDKVersion());
+		// TODO: 2019/1/19 ZCQ TEST
+		//        initBean.setDeviceId(PhoneUtil.getInstance().getPhoneImei(mContext));
+		initBean.setDeviceModel(PhoneUtil.getInstance().getPhoneModel());
+		initBean.setDeviceName(TextUtils.isEmpty(android.os.Build.DEVICE) ? "" : android.os.Build.DEVICE);
+		initBean.setUiMode(PhoneUtil.getInstance().isTablet(mContext) ? "pad" : "phone");
+		initBean.setOperatorName("移动");
+		initBean.setConnectionType("4g");
+		initBean.setScreenWidth(PhoneUtil.getInstance().getPhoneWidth(mContext));
+		initBean.setScreenHeight(PhoneUtil.getInstance().getPhoneHeight(mContext));
+		baseEmitData.setData(initBean);
+	}
+
+	private WYAWebChromeClient wyaWebChromeClient() {
+		return new WYAWebChromeClient(this);
+	}
+
+	private WYAWebViewClient wyaWebViewClient() {
+		return new WYAWebViewClient(this);
+	}
+
+	public <T> void send(String name, T t) {
+		name = "'" + name + "'";
+		BaseEmitData<T> result = new BaseEmitData<>();
+		result.setData(t);
+		BridgeUtil.loadJsUrl(this, name, new Gson().toJson(result));
+	}
+
+	public <T> void send(int id, T t) {
+		BaseEmitData<T> result = new BaseEmitData<>();
+		result.setData(t);
+		BridgeUtil.loadJsUrl(this, String.valueOf(id), new Gson().toJson(result));
+	}
+
+	public void register(String name, JsCallBack callBack) {
+		taskMap.put(name, callBack);
+	}
+
+	public void unRegister() {
+		taskMap.clear();
+		taskMap = null;
+	}
+
+	public void handlerReturnData(String url) {
+		String[] split = url.split("[?]");
+		String name = split[0];
+		JsCallBack callBack = taskMap.get(name);
+		if (callBack != null) {
+			String id = split[1].replace("id=", BridgeUtil.EMPTY);
+			BridgeUtil.getParam(this, id, callBack);
+		}
+	}
+
+	public BaseEmitData<InitBean> getBaseEmitData() {
+		return baseEmitData;
+	}
+
+	public void setBaseEmitData(BaseEmitData<InitBean> baseEmitData) {
+		this.baseEmitData = baseEmitData;
+	}
+
+	private void setKeyBoardListener() {
+		this.getRootView().getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+
+			@Override
+			public void onGlobalLayout() {
+				// 应用可以显示的区域。此处包括应用占用的区域，包括标题栏不包括状态栏
+				Rect r = new Rect();
+				WYAWebView.this.getRootView().getWindowVisibleDisplayFrame(r);
+				// 键盘最小高度
+				int minKeyboardHeight = 150;
+				// 获取状态栏高度
+				int statusBarHeight = getStatusBarHeight(mContext);
+				// 屏幕高度,不含虚拟按键的高度
+				int screenHeight = ScreenUtil.getScreenHeight(mContext) - statusBarHeight;
+				// 在不显示软键盘时，height等于状态栏的高度
+				int height = screenHeight - (r.bottom - r.top);
+				Keyboard keyboard = new Keyboard();
+				keyboard.setHeight(height);
+				if (ShowKeyboard) {
+					// 如果软键盘是弹出的状态，并且height小于等于状态栏高度，
+					// 说明这时软键盘已经收起
+					if (height - statusBarHeight < minKeyboardHeight) {
+						ShowKeyboard = false;
+						send(Keyboard.EVENT_KEYBOARD_HIDE, keyboard);
+					}
+				} else {
+					// 如果软键盘是收起的状态，并且height大于状态栏高度，
+					// 说明这时软键盘已经弹出
+					if (height - statusBarHeight > minKeyboardHeight) {
+						ShowKeyboard = true;
+						send(Keyboard.EVENT_KEYBOARD_SHOW, keyboard);
+					}
+				}
+			}
+		});
+	}
+
 }
