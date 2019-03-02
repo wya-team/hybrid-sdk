fc386222e296b389d7b80b17573f03220d805e23
李世航 <1228506851@qq.com>
2019-03-02 19:42:02 +0800

    重定向完成，当前页面刷新成功

--- ios/sdk/WYAWebView/WYAWebView/Sources/WYAWebView.swift
+++ ios/sdk/WYAWebView/WYAWebView/Sources/WYAWebView.swift
@@ -128,34 +128,37 @@ extension WYAWebView {
         let bund = Bundle(for: classForCoder)
         let websitePath = bund.path(forResource: "dist", ofType: nil)
 
+
+        // ^/.*\\.html$
+//                let arr = ["event", "const", "methods", "assists"]
+//                for string in arr {
+//                    self.webServer.addHandler(
+//                        forMethod: "GET",
+//                        pathRegex: string,
+//                        request: GCDWebServerRequest.self,
+//                        processBlock: { (request) -> GCDWebServerResponse? in
+//                            print(request)
+//
+//                            let jsString = bund.path(forResource: "dist/index", ofType: "html")
+//
+//                            var jsPath: String?
+//                            do {
+//                                jsPath = try String(contentsOfFile: jsString!)
+//
+//                                return GCDWebServerDataResponse(html: jsPath!)
+//                            } catch {
+//                                print(error)
+//                                return GCDWebServerDataResponse(redirect: self.webServer.serverURL!, permanent: false)
+//                            }
+//
+//                    })
+//                }
+
+
         // 先设置个默认的handler处理静态文件（比如css、js、图片等）
         webServer.addGETHandler(forBasePath: "/", directoryPath: websitePath!,
                                 indexFilename: nil, cacheAge: 3600,
                                 allowRangeRequests: true)
-        // ^/.*\\.html$
-                let arr = ["event", "const", "methods", "assists"]
-                for string in arr {
-                    self.webServer.addHandler(
-                        forMethod: "GET",
-                        pathRegex: string,
-                        request: GCDWebServerRequest.self,
-                        processBlock: { (request) -> GCDWebServerResponse? in
-                            print(request)
-                            
-                            let jsString = bund.path(forResource: "dist/index", ofType: "html")
-
-                            var jsPath: String?
-                            do {
-                                jsPath = try String(contentsOfFile: jsString!)
-
-                                return GCDWebServerDataResponse(html: jsPath!)
-                            } catch {
-                                print(error)
-                                return GCDWebServerDataResponse(redirect: self.webServer.serverURL!, permanent: false)
-                            }
-
-                    })
-                }
 
         // HTTP请求重定向（/从定向到/index.html）
         self.webServer.addHandler(
@@ -168,50 +171,15 @@ extension WYAWebView {
                 return GCDWebServerResponse(redirect: url!, permanent: false)
         })
 
-        //            webServer.addHandler(forMethod: "GET", pathRegex: string, request: GCDWebServerRequest.self) { (request) -> GCDWebServerResponse? in
-        //                if request.path == "index.html" {
-        //                    return GCDWebServerResponse(redirect: request.url, permanent: false)
-        //                }
-        //
-        //                let jss = bund.path(forResource: "dist", ofType: nil)
-        //                print(jss as Any)
-        //
-        //                let jsString = bund.path(forResource: "dist/index", ofType: "html")
-        //
-        //                var jsPath : String?
-        //                do {
-        //                    jsPath = try String(contentsOfFile: jsString!)
-        //                } catch {
-        //                    print(error)
-        //                }
-        //
-        //
-        //                let path = jss! + request.path
-        //
-        //                let response = GCDWebServerDataResponse(html: jsPath!)
-        //
-        //                if FileManager.default.fileExists(atPath: path) {
-        //
-        //                    response?.statusCode = 200
-        //
-        //                }else {
-        //
-        //
-        //                }
-        //                return response
-        //            }
-        //        }
-        //
-//        "ConnectionClass": String(describing: WYAConenct.self)
+//
         let options: Dictionary<String, Any> = ["Port": 8080,
                                                 "AutomaticallySuspendInBackground": false,
                                                 "BindToLocalhost": true,
-                                                ]
+                                                "ConnectionClass":WYAConenct.self]
         do {
             try self.webServer.start(options: options)
             // 打开网页
-            self.loadUrl(url: (self.webServer.serverURL?.absoluteString)!)
-            //            self.loadUrl(url: "http://www.baidu.com")
+            self.loadUrl(url: "http://localhost:8080/")
         } catch {
             print(error)
         }
@@ -340,14 +308,7 @@ extension WYAWebView: WKNavigationDelegate, WKUIDelegate, WKScriptMessageHandler
     /// 拿到响应后决定是否允许跳转
     public func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Swift.Void) {
         //        self.webManager.backBtnPressed()
-        let urlRespon = navigationResponse.response as! HTTPURLResponse
-        //
-        if urlRespon.statusCode == 200 {
 
-        }else if urlRespon.statusCode == 404 {
-//            loadUrl(url: (webServer.serverURL?.absoluteString)!)
-
-        }
         decisionHandler(WKNavigationResponsePolicy.allow)
     }
 
@@ -572,6 +533,10 @@ extension WYAWebView {
 
 
 class WYAConenct: GCDWebServerConnection {
+
+    override init() {
+        super.init()
+    }
     override func rewriteRequest(_ url: URL, withMethod method: String, headers: [String : String]) -> URL {
         return super.rewriteRequest(url, withMethod: method, headers: headers)
     }
@@ -581,6 +546,19 @@ class WYAConenct: GCDWebServerConnection {
     }
 
     override func overrideResponse(_ response: GCDWebServerResponse, for request: GCDWebServerRequest) -> GCDWebServerResponse {
+        if response.statusCode == 404 {
+            let bund = Bundle(for: classForCoder)
+            let jsString = bund.path(forResource: "dist/index", ofType: "html")
+
+            var jsPath: String?
+            do {
+                jsPath = try String(contentsOfFile: jsString!)
+
+                return GCDWebServerDataResponse(html: jsPath!)!
+            }catch{
+                print(error)
+            }
+        }
         return super.overrideResponse(response, for: request)
     }
 
