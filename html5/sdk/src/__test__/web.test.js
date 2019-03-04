af5c66d96aa1eaad8e17433fa920b042f75a7774
Mr.Z <zrd0921@qq.com>
2019-03-04 10:26:31 +0800

    add: registerModule and requireModule

--- html5/sdk/src/__test__/web.test.js
+++ html5/sdk/src/__test__/web.test.js
@@ -4,52 +4,52 @@ describe('web.js', () => {
 	window.WYAJSBridge = WYAJSBridge;
 	let id = WYAJSBridge.count;
 
-	// test('验证api', () => {
-	// 	expect(typeof wya).toBe('object');
-	// 	expect(typeof wya.version).toBe('string');
-	// 	expect(typeof wya.invoke).toBe('function');
-	// 	expect(typeof wya.on).toBe('function');
-	// 	expect(typeof wya.once).toBe('function');
-	// 	expect(typeof wya.first).toBe('function');
-	// 	expect(typeof wya.last).toBe('function');
-	// 	expect(typeof wya.off).toBe('function');
-	// 	expect(typeof wya.onError).toBe('function');
-	// });
-	// test('验证ready', () => {
-	// 	wya.ready(() => {
-	// 		expect(1).toBe(1);
-	// 	});
+	test('验证api', () => {
+		expect(typeof wya).toBe('object');
+		expect(typeof wya.version).toBe('string');
+		expect(typeof wya.invoke).toBe('function');
+		expect(typeof wya.on).toBe('function');
+		expect(typeof wya.once).toBe('function');
+		expect(typeof wya.first).toBe('function');
+		expect(typeof wya.last).toBe('function');
+		expect(typeof wya.off).toBe('function');
+		expect(typeof wya.onError).toBe('function');
+	});
+	test('验证ready', () => {
+		wya.ready(() => {
+			expect(1).toBe(1);
+		});
 
-	// 	// 模拟触发一次
-	// 	WYAJSBridge.emit('_ready_', {
-	// 		status: 1,
-	// 		data: {
-	// 			appVersion: '1.0.0' 
-	// 		}
-	// 	});
+		// 模拟触发一次
+		WYAJSBridge.emit('_ready_', {
+			status: 1,
+			data: {
+				appVersion: '1.0.0' 
+			}
+		});
 
-	// });
-	// test('验证error', () => {
-	// 	wya.ready(() => {
-	// 		expect(1).toBe(1);
-	// 	});
+	});
+	test('验证error', () => {
+		wya.ready(() => {
+			expect(1).toBe(1);
+		});
 
-	// 	wya.onError((res) => {
-	// 		expect(res.msg).toBe('error');
-	// 	});
+		wya.onError((res) => {
+			expect(res.msg).toBe('error');
+		});
 
-	// 	wya.on('_error_', (res) => {
-	// 		expect(res.msg).toBe('error');
-	// 	});
+		wya.on('_error_', (res) => {
+			expect(res.msg).toBe('error');
+		});
 
-	// 	// Native 模拟触发
-	// 	WYAJSBridge.emit('_error_', {
-	// 		status: 0,
-	// 		msg: 'error',
-	// 		data: {}
-	// 	});
+		// Native 模拟触发
+		WYAJSBridge.emit('_error_', {
+			status: 0,
+			msg: 'error',
+			data: {}
+		});
 
-	// });
+	});
 
 	test('验证event', () => {
 		wya.on('batteryLow', (res) => {
@@ -91,6 +91,35 @@ describe('web.js', () => {
 		id++; // 触发后增加''
 		
 		expect(WYAJSBridge.nativeEvents.includes('batteryLow')).toBe(false);
+		expect(WYAJSBridge.count).toBe(id);
+	});
+
+	test('验证requireModule', () => {
+		let navigator = wya.requireModule('navigator');
+		navigator.push({ user: 'wya' })
+			.then((res) => {
+				expect(res.type).toBe("3g");
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+		expect(typeof WYAJSBridge.store[id]).toBe("object");
+		expect(WYAJSBridge.store[id].scheme).toBe("navigator/push");
+
+		let params = JSON.parse(WYAJSBridge.getParams(id));		
+		expect(params.user).toBe("wya");
+
+		WYAJSBridge.emit(id, { 
+			status: 1,
+			data: {
+				type: '3g'
+			}
+		});
+
+		expect(typeof WYAJSBridge.store[id]).toBe("undefined");
+
+		id++; // 触发后增加
+
 		expect(WYAJSBridge.count).toBe(id);
 	});
 });
