9c09c781dd7a3b173474a515c71f0d5698a64389
xudonglin <465715784@qq.com>
2019-03-06 10:14:02 +0800

    add permission

--- android/sdk/src/main/java/com/wya/hybrid/HybridManager.java
+++ android/sdk/src/main/java/com/wya/hybrid/HybridManager.java
@@ -854,6 +854,17 @@ public class HybridManager implements JsCallBack {
 			}
 		} else {
 			Toast.makeText(mContext, "没有浮窗权限！", Toast.LENGTH_SHORT).show();
+			Intent localIntent = new Intent();
+			localIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+			if (Build.VERSION.SDK_INT >= 9) {
+				localIntent.setAction("android.settings.APPLICATION_DETAILS_SETTINGS");
+				localIntent.setData(Uri.fromParts("package", mContext.getPackageName(), null));
+			} else if (Build.VERSION.SDK_INT <= 8) {
+				localIntent.setAction(Intent.ACTION_VIEW);
+				localIntent.setClassName("com.android.settings", "com.android.settings.InstalledAppDetails");
+				localIntent.putExtra("com.android.settings.ApplicationPkgName", mContext.getPackageName());
+			}
+			mContext.startActivity(localIntent);
 		}
 	}
 
