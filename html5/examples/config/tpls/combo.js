4aee70d067052e6c3a03d699d930fa9ae0b60f6f
Mr.Z <zrd0921@qq.com>
2019-03-06 16:17:33 +0800

    fix

--- html5/examples/config/tpls/combo.js
+++ html5/examples/config/tpls/combo.js
@@ -59,7 +59,7 @@ module.exports = (category, filename) => {
 
 	let contents = '';
 	contents += `import wya from 'wya-js-sdk';\n`;
-	contents += `import Toasts from '@common/toasts/toasts';\n`;
+	contents += `import Toasts from '@common/toast/toast';\n`;
 	contents += `import markdown from '@docs/${category}/${filename}.md';\n`;
 	contents += `\n`;
 	contents += tpl(category, filename, scheme);
